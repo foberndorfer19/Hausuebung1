c95bca281c4df7fe563f11be8ec41ba6d47e2ace
Fabian Oberndorfer <OberndorferF190097@sus.htl-grieskirchen.at>
2021-09-28 15:18:55 +0200

    commit6

--- Hausuebung1/src/net/htlgkr/OberndorferF190097/hausuebung1/EratosthenesPrimeSieve.java
+++ Hausuebung1/src/net/htlgkr/OberndorferF190097/hausuebung1/EratosthenesPrimeSieve.java
@@ -7,13 +7,13 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     private int o;
 
+    boolean[] gestrichen = getGestrichen();
     public EratosthenesPrimeSieve(int o) {
         this.o = o;
     }
     
     @Override
     public boolean isPrime(int p) {
-        boolean gestrichen[] = getGestrichen();
         
         if(!gestrichen[p]){
             return true;
@@ -24,7 +24,6 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
-        boolean gestrichen[] = getGestrichen();
         
         for(int i = 2;i<gestrichen.length;i++){
             if(!gestrichen[i]){
@@ -38,7 +37,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
         
         for(int i = 2;i<gestrichen.length;i++){
             if(!gestrichen[i]){
-                int x = i*i;
+                int x = i*2;
                 for(int j = 0;j<o;j++){
                     if(x == j){
                         gestrichen[j] = true;
@@ -54,7 +53,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     
     public void ngPrimeCalculation(){
         int ng[] = new int[o/2-1];
-        boolean gestrichen[] = getGestrichen();
+        
         List<String> primeNumbers = new ArrayList<>();
         
         for(int i = 0;i<gestrichen.length;i++){
@@ -75,7 +74,7 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
             for(int j = 0;j<primeNumbers.size();j++){
                 for(int x = 0;x<primeNumbers.size();x++){
                     if(ngNumber == Integer.parseInt(primeNumbers.get(j))+Integer.parseInt(primeNumbers.get(x))){
-                        System.out.println(ngNumber + "summe: "+ngNumber + " = " + Integer.parseInt(primeNumbers.get(j)) + " + " + Integer.parseInt(primeNumbers.get(x)));
+                        System.out.println(ngNumber + " summe: "+ngNumber + " = " + Integer.parseInt(primeNumbers.get(j)) + " + " + Integer.parseInt(primeNumbers.get(x)));
                     }
                 }
             }
--- Hausuebung1/src/net/htlgkr/OberndorferF190097/hausuebung1/Main.java
+++ Hausuebung1/src/net/htlgkr/OberndorferF190097/hausuebung1/Main.java
@@ -1,9 +1,59 @@
 package net.htlgkr.OberndorferF190097.hausuebung1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner userInputReader = new Scanner(System.in);
+        boolean run = true;
         
+        System.out.println("Select upper limit:");
+        int o = userInputReader.nextInt();
+        userInputReader.nextLine();
+        
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(o);
+        
+        while(run){
+            menue();
+            int userInput = userInputReader.nextInt();
+            userInputReader.nextLine();
+            
+            switch(userInput){
+                case 1:
+                    System.out.println("Select number to check:");
+                    int n = userInputReader.nextInt();
+                    userInputReader.nextLine();
+                    
+                    if(eps.isPrime(n)){
+                        System.out.println("It is a Prime number!");
+                    }
+                    else{
+                        System.out.println("It is not a Prime number!");
+                    }
+                    break;
+                case 2:
+                    eps.printPrimes();
+                    break;
+                case 3:
+                    eps.ngPrimeCalculation();
+                    break;
+                case 4:
+                    run = false;
+                    break;    
+                default:
+                    System.out.println("Error!");
+                    break;
+            }
+        }
     }
     
+    public static void menue(){
+        System.out.println("1 ... isPrime");
+        System.out.println("2 ... printPrimes");
+        System.out.println("3 ... ngPrimeCalculation");
+        System.out.println("4 ... End ");
+        
+        System.out.println("Select:");
+    }
 }
